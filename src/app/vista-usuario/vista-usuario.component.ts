13ff18a998bd52cc7a7bb3feda04aa0c2aead0b4
USERSAD\dlopezru <se_bat_94@hotmail.com>
2018-03-26 17:28:27 +0200

    Redimensionar header al hacer scroll y cambio de diseño

--- src/app/vista-usuario/vista-usuario.component.ts
+++ src/app/vista-usuario/vista-usuario.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { PersonaServiceService } from '../Services/persona-service.service';
 import { Persona } from '../Entidades/persona';
 import { Observable } from 'rxjs/Observable';
@@ -8,18 +8,32 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './vista-usuario.component.html',
   styleUrls: ['./vista-usuario.component.css']
 })
-export class VistaUsuarioComponent implements OnInit {
+export class VistaUsuarioComponent implements OnInit, OnDestroy {
 
+    persona: Persona;
+    isOnTop = true;
 
-  persona: Persona;
+    constructor(private pService: PersonaServiceService) {
+    }
 
-  constructor(private pService: PersonaServiceService) {
-  }
+    ngOnInit() {
+        this.pService.getPerson(1).subscribe(res => {
+        this.persona = res;
+        });
 
-  ngOnInit() {
-     this.pService.getPerson(1).subscribe(res => {
-       this.persona = res;
-     });
-  }
+        window.addEventListener('scroll', this.scroll, true);
+    }
+
+    ngOnDestroy() {
+        window.removeEventListener('scroll', this.scroll, true);
+    }
+
+    scroll = (): void => {
+        const top_of_page = window.scrollY;
+        if (top_of_page === 0) {
+            this.isOnTop = true;
+        } else { this.isOnTop = false; }
+
+    }
 
 }
